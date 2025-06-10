20ea537ed07b8640ceb7ca82d10c2a79be10aa83
sonakshiyadv <sonakshiyd29@gmail.com>
2025-06-11 00:58:55 +0530

    something new

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// ----------------------
+$('#hamburger').click(function() {
+    $('.hamburger_menu').addClass('myClass');
+  });
+  $('.your_class').click(function() {
+    $('.hamburger_menu').addClass('yourclass');
+  });
+  
+  
+
+// Alignment issues fix
+// Add content to projects 
+// download resume 
+// make contact us page working
+// effect typewriting
+// hamburger menu not working
+
+// regex - regular expressions
